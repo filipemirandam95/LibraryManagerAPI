82c51b17a19a0dbb907b9926a35d840b633ddffe
Filipe <filipemad@yahoo.com.br>
2024-09-09 16:27:02 -0300

    Initial commit

---
+++ src/main/java/com/example/LibraryManagerAPI/LibraryManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.LibraryManagerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LibraryManagerAPI/LibraryManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LibraryManagerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
